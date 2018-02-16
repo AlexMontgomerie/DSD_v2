c9722e00073d4abcd5efd7e5cf7b26bde935a013
Alex Montgomerie <AlexMontgomerie@github.com>
2018-02-16 16:15:37 +0000

    started on combinational floating point multiplier TODO: work on testbench

--- design_files/ahfp_add.v
+++ design_files/ahfp_add.v
@@ -3,7 +3,7 @@
 // 
 // Design Notes:
 // - multi-cycle 
-//
+// - refere to altera's ug_embedded_ip for info
 // ############################################
 
 module ahfp_mult_combi(
@@ -18,17 +18,17 @@ input	[31:0] datab;
 //output
 output	[31:0] result;
 
-wire	[23:0] 	a_m, b_m, z_m;
-wire	[9:0]  	a_e, b_e, z_e;
-wire	   		a_s, b_s, z_s;
+wire	[22:0] 	a_m, b_m, z_m;
+wire	[7:0]  	a_e, b_e, z_e;
+wire   		a_s, b_s, z_s;
 
 //parameters
 
 
 //get fp fields
 //initialise mantissa
-assign a_m = dataa[22:0];
-assign b_m = datab[22:0];
+assign a_m = {1'b1,dataa[22:1]};
+assign b_m = {1'b1,datab[22:1]};
 	
 //initialise exponent
 assign a_e = dataa[30:23];
@@ -40,12 +40,17 @@ assign b_s = datab[31];
 
 //assign output sign
 assign z_s = a_s ^ b_s;
-//assign exponent
-assign z_e = a_e + b_e - 127;
-//get mantissa
-assign z_m = a_m + a_m;
-//round mantissa result
 
+//assign exponent
+wire [8:0] exp_tmp;
+assign exp_tmp = a_e + b_e;
+
+//get mantissa
+wire [46:0] man_tmp;
+assign man_tmp = a_m * b_m;
+
+assign z_e = man_tmp[46] ? (exp_tmp-9'd126) : (exp_tmp-9'd127);
+assign z_m = man_tmp[46] ? (man_tmp[45:23]) : (man_tmp[45:22]);
 
 //assign output
 assign result = z_s << 31 | z_e << 23 | z_m;
