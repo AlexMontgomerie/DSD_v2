2c0f07d57bd2f8a5c43f70d5030fa143f5deeaba
Henry Eshbaugh <he915@ic.ac.uk>
2018-03-05 22:06:41 +0000

    lzd testbench

--- design_files/ahfp_sub/ahfp_lzd_tb.v
+++ design_files/ahfp_sub/ahfp_lzd_tb.v
@@ -2,15 +2,15 @@
 // testbench reference: https://people.ece.cornell.edu/land/courses/ece5760/Verilog/LatticeTestbenchPrimer.pdf
 
 `timescale 1ns / 1ns
-module test_ahfp_add;
+module test_ahfp_lzd;
 
-reg clk;
-reg [31:0] dataa,datab,result_correct;
-wire [31:0] result;
+reg  [47:0] in;
+wire [6:0]  out;
+wire        valid;
 
-ahfp_add dut (	.dataa(dataa),
-		.datab(datab),
-		.result(result));
+ahfp_lzd48 dut ( .in (in),
+	         .out(out),
+		 .valid(valid))
 
 initial // Clock generator
   begin
@@ -22,10 +22,7 @@ initial // Clock generator
 initial
 	begin
 		//test 1 (time 0ns)
-		dataa 			= 32'h00000000;
-		datab			= 32'h00000000;
-		result_correct	= 32'h00000000;
-
+		// TODO testbenches
 		//test 2 (time 20ns)
 		#20 dataa 		= 32'h3F800000;
 		datab			= 32'h40000000;
