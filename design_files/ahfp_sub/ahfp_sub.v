2dd3776eecbeae945f478d5a47c4a50eb649c722
Henry Eshbaugh <he915@ic.ac.uk>
2018-03-05 21:40:42 +0000

    LZD implementation

--- design_files/ahfp_sub/ahfp_sub.v
+++ design_files/ahfp_sub/ahfp_sub.v
@@ -6,6 +6,72 @@
 // - refere to altera's ug_embedded_ip for info
 // ############################################
 
+module lzd4(in, out, valid)
+	input  wire [3:0] in;
+	output wire [1:0] out;
+	output wire valid;
+	assign {out, valid} = in[3] ? {2'b11, 1'b1} :
+		              in[2] ? {2'b10, 1'b1} :
+		              in[1] ? {2'b01, 1'b1} :
+		              in[0] ? {2'b00, 1'b1} :
+		                      {2'b00, 1'b0} ; // all zero
+endmodule
+
+module lzd8(in, out, valid)
+	wire [7:0] in;
+	wire [2:0] out;
+	wire valid;
+	wire [1:0] res1, res2;
+	wire v1, v2;
+	lzd4 lo(in[3:0], res1, v1);
+	lzd4 hi(in[7:4], res2, v2);
+	assign valid = v1 | v2;
+	assign out = v2 ? {1'b1, res2} : {0'b1, res1};
+endmodule
+
+module lzd16(in, out, valid)
+	wire [15:0] in;
+	wire [3:0] out;
+	wire valid;
+	wire [2:0] res1, res2;
+	wire v1, v2;
+	lzd8 lo(in[7:0], res1, v1);
+	lzd8 hi(in[15:8], res2, v2);
+	assign valid = v1 | v2;
+	assign out = v2 ? {1'b1, res2} : {0'b1, res1};
+endmodule
+
+module lzd32(in, out, valid)
+	wire [31:0] in;
+	wire [4:0] out;
+	wire valid;
+	wire [3:0] res1, res2;
+	wire v1, v2;
+	lzd16 lo(in[15:0], res1, v1);
+	lzd16 hi(in[31:16], res2, v2);
+	assign valid = v1 | v2;
+	assign out = v2 ? {1'b1, res2} : {0'b1, res1};
+endmodule
+
+module lzd64(in, out, valid)
+	wire [63:0] in;
+	wire [5:0] out;
+	wire valid;
+	wire [4:0] res1, res2;
+	wire v1, v2;
+	lzd32 lo(in[31:0], res1, v1);
+	lzd32 hi(in[63:32], res2, v2);
+	assign valid = v1 | v2;
+	assign out = v2 ? {1'b1, res2} : {0'b1, res1};
+endmodule
+
+module lzd48(in, out, valid)
+	wire [47:0] in;
+	wire [5:0] out;
+	wire valid;
+	lzd64 foo({in, 16'h0000}, out, valid);
+endmodule
+
 module ahfp_sub(
 	dataa,
 	datab,
@@ -116,7 +182,7 @@ assign {z_m_overflow,z_m_tmp,z_e_tmp} = (m_tmp[47] == 1'b1) ? {m_tmp[46:24] + m_
 				(m_tmp[3] == 1'b1)  ? {{1'd0,m_tmp[2:0],20'd0} , e_tmp - e_diff - 8'd20 } :
 				(m_tmp[2] == 1'b1)  ? {{1'd0,m_tmp[1:0],21'd0} , e_tmp - e_diff - 8'd21 } :
 				(m_tmp[1] == 1'b1)  ? {{1'd0,m_tmp[0:0],22'd0} , e_tmp - e_diff - 8'd22 } :
-				(m_tmp[0] == 1'b1)  ? {24'd0 					, e_tmp - e_diff - 8'd23 } :
+				(m_tmp[0] == 1'b1)  ? {24'd0 		       , e_tmp - e_diff - 8'd23 } :
 									  {24'd0,8'd0} ; 
 				
 /*
