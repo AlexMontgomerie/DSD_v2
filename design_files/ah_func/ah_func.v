e854331d349fa8dd04ee026d85a90eced8b319a1
Henry Eshbaugh <he915@ic.ac.uk>
2018-03-13 23:18:10 +0000

    ah_func, FIFO parallel to pipeline, LZD optimization

--- design_files/ah_func/ah_func.v
+++ design_files/ah_func/ah_func.v
@@ -1,21 +1,22 @@
 `include "../ahfp_mul_multi/ahfp_mul_multi.v"
 `include "../ahfp_mod_pi/ahfp_mod_pi.v"
-`include "../ahfp_pipeline_buffer/ahfp_pipeline_buffer.v"
 `include "../ahfp_floor_divide_four/ahfp_floor_divide_four.v"
 `include "../ahfp_add_sub_multi/ahfp_add_sub_multi.v"
 `include "../ahfp_float_2_fixed/ahfp_float_2_fixed.v"
 `include "../ahfp_cordic_fixed/ahfp_cordic_fixed.v"
 `include "../ahfp_fixed_2_float/ahfp_fixed_2_float.v"
+`include "../ahfp_mult/ahfp_mult.v"
 
 module ah_func(	clk,
-				dataa,
-				datab,
-				result
-			);
+		dataa,
+		datab,
+		result);
 			
-parameter thirty_two	= 32'h00000000;
-parameter fixed_one 	= 32'h20000000;
-parameter fixed_zero 	= 32'h00000000;
+parameter thirty_two	 = 32'h42000000;
+parameter fixed_one 	 = 32'h20000000;
+parameter fixed_zero 	 = 32'h00000000;
+parameter float_one_half = 32'h3f000000;
+parameter nstages        = 56;
 		
 input clk;
 input 	[31:0] dataa, datab;
@@ -25,25 +26,33 @@ output 	[31:0] result;
 
 wire [31:0] a_buf, b_buf;
 
+reg  [31:0] xfifo[0:nstages-1];
+genvar i;
+generate
+	for (i = 0; i < nstages-1; i=i+1)
+	begin:xfifo_gen
+		always @(posedge clk) begin
+			xfifo[i+1] <= xfifo[i];
+		end
+	end
+endgenerate
+
 `define STAGES 20
+`include "../ahfp_pipeline_buffer/ahfp_pipeline_buffer.v"
 ahfp_pipeline_buffer buf_a (clk, dataa, a_buf);
 ahfp_pipeline_buffer buf_b (clk, datab, b_buf);		
 `undef STAGES							
 //Stage 1 => get the floor result
 
 wire 	[31:0] floor_res;
-reg		[31:0] floor_res_reg;
 
 ahfp_floor_divide_four floor (dataa, floor_res);
-always @(posedge clk) begin
-	floor_res_reg <= floor_res;
-end
 
 //Stage 2 => subtract floor result from 
 
 wire [31:0] floor_sub_res;
 
-ahfp_add_sub_multi sub (clk,floor_res_reg,thirty_two,floor_sub_res);
+ahfp_add_sub_multi sub (clk,floor_res, thirty_two, floor_sub_res);
 		
 //Stage 3 => cos of the floor sub result
 
@@ -59,6 +68,8 @@ always @(posedge clk) begin
 	fixed_theta_in_reg <= fixed_theta_in;
 end
 
+// => Stage 4: calculate cos
+
 //calculate cos
 wire	[31:0] cos;
 ahfp_cordic_fixed cordic (clk,fixed_one,fixed_zero,fixed_theta_in_reg,cos);
@@ -71,21 +82,21 @@ always @(posedge clk) begin
 	float_cos_out_reg <= float_cos_out;
 end
 
-//Stage 4 => multiply with cos and add 1
+//Stage 5 => multiply with cos and add 1
 
 //buffer x again, after getting it
 `define STAGES 5
+`include "../ahfp_pipeline_buffer/ahfp_pipeline_buffer.v"
 wire [31:0] x_buf;
 `undef STAGES
 
-wire [31:0] mul_cos_res;
-wire [31:0] cos_add_one_res;
-ahfp_mul_multi mul_cos (clk, float_cos_out_reg, {a_buf[31],a_buf[30:23]+1,a_buf[22:0]}, mul_cos_res);
-ahfp_add_sub_multi cos_add_one (clk, mul_cos_res, float_one, cos_add_one_res); 
+wire [31:0] mul_cos_res, xcos_res;
+wire [31:0] cos_add_half_res;
 
-//TODO: 
-// - do the final multiplication by initial x (use a buffer)
-// - assign the result 
-// - get a value for thirty two
+ahfp_mul_multi mul_cos (clk, float_cos_out_reg, {a_buf[31],a_buf[30:23]+1,a_buf[22:0]}, mul_cos_res);
+ahfp_mul_multi mul_xcos(clk, mul_cos_res, xfifo[N-7], xcos_res);
+ahfp_add_sub_multi cos_add_half (clk, xcos_res, float_one_half, cos_add_half_res); 
+
+ahfp_mul_multi mulx(clk, cos_add_half_res, xfifo[N-1], result);
 
 endmodule
--- design_files/ahfp_lzd/ahfp_lzd48.v
+++ design_files/ahfp_lzd/ahfp_lzd48.v
@@ -10,5 +10,13 @@ module ahfp_lzd48(in, out, valid);
 	input 	wire [47:0] in;
 	output 	wire [5:0] 	out;
 	output	wire valid;
-	ahfp_lzd64 msb_finder ({16'h0000,in}, out, valid);
+	wire [5:0] res1;
+        wire [4:0] res2;
+	wire v1, v2;
+
+	ahfp_lzd32 lo(in[31:0] , res1     , v1);
+	ahfp_lzd16 hi(in[47:32], res2[4:0], v2);
+
+	assign valid = v1 | v2;
+	assign out = v2 ? {2'b10, res2} : {1'b0, res1};
 endmodule
